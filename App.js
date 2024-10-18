071bfd888d8eaca87d3ba12c9a6485d5049c13d4
AbdulAhad83392 <ahad83392@gmail.com>
2024-10-18 12:45:50 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,165 @@
+// import './App.css';
+// import 'swiper/css';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+// project part start
+import Main from './Project/Main';
+import './Project/Project.css'  
+import Portfoilo from './Project/Portfoilo';
+import About from './Project/About';
+import Contact from './Project/Contact';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // import Second from './Api\'s/Second';
+  // import Hello from './Theme/Hello';
+  // import './Theme/Theme.css'
+  // import './Theme/Nav.css'
+  // import Nav from './Theme/Nav';
+
+  // import Slider1 from './Slider/Slider'; 
+  // import Slider2 from './Slider/Slider2';
+  // // import Home from './Routes/Home';
+  // // import About from './Routes/About'
+  // // import Team from './Routes/Team'
+  // // import Services from './Routes/Services'
+  // // import Portfolio from './Routes/Portfolio'
+  // // import Contact from './Routes/Contact'
+  // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+  // // import Error from './Routes/Error';
+  // import Main from './Main/Main';
+  // import Navv from './Main/Navv';
+  // import First from './Main/First';
+  // import Uiux from './Main/Uiux';
+  // import Skill from './Main/Skill';
+  // import Resume from './Main/Resume';
+  // import About from './Main/About';
+  // import Portfolio from './Main/Portfolio';
+  // import Services from './Main/Services';
+  // import Testimnial from './Main/Testimnial';
+  // import Contact from './Main/Contact';
+
+function App() {
+    
+  
+    return (
+      <div classNameName="App">
+
+        <Main />
+      
+        <Portfoilo />
+
+        <About />
+        
+        <Contact/>  
+      
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+        {/* <Main /> 
+      <Router>
+        {/* <Navv /> */}
+        {/* <Routes>
+
+          <Route path='/' element={<First />} />
+          <Route path='/first' element={<First />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/uiux' element={<Uiux />} />
+          <Route path='/skill' element={<Skill />} />
+          <Route path='/resume' element={<Resume />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/testimnial' element={<Testimnial />} />
+          <Route path='/contact' element={<Contact />} />
+
+        </Routes>
+
+      </Router> */ }
+
+        {/* <Router>
+      <Home/>
+        <Routes>
+          <Route path='/' element={<About/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/team' element={<Team/>}/>
+          <Route path='/services' element={<Services/>}/>
+          <Route path='/portfolio' element={<Portfolio/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='*' element={<Error/>}/>
+        </Routes>
+
+      </Router> */}
+
+
+
+
+
+        {/* <div className="slider">
+        <div className="sli">
+          <h4>Slider 1</h4>
+          <Slider1 />
+        </div>
+
+
+        <div className="sli">
+          <h4>Slider 2</h4>
+          <Slider2 />
+        </div>
+      </div> */}
+
+
+        {/* <Hello/> */}
+        {/* <Nav/> */}
+
+        {/* <Api /> */}
+        {/* <Second/> */}
+      
+
+
+
+
+
+
+
+
+
+
+      </div>
+    );
+  }
+
+
+export default App; 
